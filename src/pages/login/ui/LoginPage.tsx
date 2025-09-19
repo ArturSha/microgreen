66ddce8aaef0129a6d60e4a4b37f308240e2b8eb
Artur <mr.arturio06@gmail.com>
2025-09-19 14:49:59 +0300

    draft login page

--- src/pages/login/ui/LoginPage.tsx
+++ src/pages/login/ui/LoginPage.tsx
@@ -1,9 +1,49 @@
+import { useForm, type SubmitHandler } from 'react-hook-form';
 import style from './LoginPage.module.css';
 
+interface InputI {
+  password: string;
+}
+
 const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    clearErrors,
+    setError,
+    resetField,
+    formState: { errors },
+  } = useForm<InputI>();
+
+  const handleClearBtn = () => {
+    resetField('password', {
+      defaultValue: '',
+    });
+  };
+
+  const onSubmit: SubmitHandler<InputI> = (data) => {
+    if (data.password === import.meta.env.VITE_PASSWORD) {
+      console.log('success');
+      clearErrors('password');
+    } else {
+      setError('password', {
+        type: 'manual',
+        message: 'Пароль не верный',
+      });
+    }
+  };
   return (
     <section className={style.loginPage}>
-      <form action=""></form>
+      <form className={style.form} onSubmit={handleSubmit(onSubmit)}>
+        <input type="password" placeholder="Введите пароль" {...register('password')} />
+        <div>
+          <button type="submit">Войти</button>
+          <button type="button" onClick={handleClearBtn}>
+            Очистить
+          </button>
+        </div>
+        {errors.password && <p>{errors.password.message}</p>}
+      </form>
     </section>
   );
 };

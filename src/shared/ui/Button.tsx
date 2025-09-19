5a6783470dbd1755de41755ff4a341cdecb3d8c9
Artur <mr.arturio06@gmail.com>
2025-09-19 15:00:59 +0300

    added Button

--- src/shared/ui/Button.tsx
+++ src/shared/ui/Button.tsx
@@ -2,11 +2,28 @@ import { Button as Btn, type ButtonProps } from '@headlessui/react';
 import classNames from 'classnames';
 import style from './Button.module.css';
 
+type Variant = 'primary' | 'secondary' | 'danger';
+
 interface ButtonI extends ButtonProps {
   className?: string;
+  variant?: Variant;
 }
 
 export const Button = (props: ButtonI) => {
-  const { className, ...rest } = props;
-  return <Btn className={classNames(style.button, className)} {...rest}></Btn>;
+  const { className, variant = 'primary', ...rest } = props;
+
+  return (
+    <Btn
+      className={classNames(
+        style.button,
+        {
+          [style.primary]: variant === 'primary',
+          [style.secondary]: variant === 'secondary',
+          [style.danger]: variant === 'danger',
+        },
+        className,
+      )}
+      {...rest}
+    />
+  );
 };

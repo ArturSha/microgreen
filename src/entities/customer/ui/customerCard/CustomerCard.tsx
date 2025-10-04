a42980f117ee7083d41a2e893f43751bd0a7629c
Artur <mr.arturio06@gmail.com>
2025-10-04 12:36:01 +0300

    added popover for customer card

--- src/entities/customer/ui/customerCard/CustomerCard.tsx
+++ src/entities/customer/ui/customerCard/CustomerCard.tsx
@@ -1,17 +1,36 @@
+import type { ReactNode } from 'react';
+import { PopoverButton, Popover, PopoverPanel } from '@/shared/ui/Popover';
 import { Text } from '@/shared/ui/Text';
 import type { Customer } from '../../model/types/customer';
 import style from './CustomerCard.module.css';
 
 interface CustomerProps {
   data: Customer;
+  children: ReactNode;
 }
 
-export const CustomerCard = ({ data }: CustomerProps) => {
-  const { name, debt } = data;
+export const CustomerCard = ({ data, children }: CustomerProps) => {
+  const { name, debt, address, contactPerson, phone, notes } = data;
   return (
-    <div className={style.customerCard}>
-      <Text as="span">{name}</Text>
-      <Text as="span">{debt}</Text>
-    </div>
+    <Popover>
+      <PopoverButton>
+        <div className={style.customerCard}>
+          <Text as="span">{name}</Text>
+          <Text as="span">{debt}</Text>
+        </div>
+      </PopoverButton>
+      <PopoverPanel anchor="bottom">
+        <div className={style.popoverContainer}>
+          <div>
+            <Text>{name} </Text>
+            {address && <Text>{address}</Text>}
+            {contactPerson && <Text>{contactPerson}</Text>}
+            {phone && <Text>{phone}</Text>}
+            {notes && <Text>{notes}</Text>}
+          </div>
+          {children}
+        </div>
+      </PopoverPanel>
+    </Popover>
   );
 };
--- src/shared/ui/Popover/PopoverButton/PopoverButton.tsx
+++ src/shared/ui/Popover/PopoverButton/PopoverButton.tsx
@@ -1,35 +1,14 @@
 import type { PopoverButtonProps } from '@headlessui/react';
 import { PopoverButton as PopoverButtonHeadless } from '@headlessui/react';
 import classNames from 'classnames';
-import type { ReactNode } from 'react';
-import Filter from '../../../assets/icons/filter.svg';
-import InfoMessage from '../../../assets/icons/messageInfo.svg';
 import style from './PopoverButton.module.css';
 
-const icons = {
-  filter: <Filter />,
-  InfoMessage: <InfoMessage />,
-  tag: null,
-  empty: null,
-} as const;
-
-interface PopoverButtonHeadlessProps extends PopoverButtonProps {
-  kind?: keyof typeof icons;
-  iconPosition?: 'left' | 'right';
-  children?: ReactNode;
-}
-
-export const PopoverButton = (props: PopoverButtonHeadlessProps) => {
-  const { kind, className, children, iconPosition = 'left', ...rest } = props;
+export const PopoverButton = (props: PopoverButtonProps) => {
+  const { className, children, ...rest } = props;
 
   return (
-    <PopoverButtonHeadless
-      className={classNames(style.popoverButton, { [style.tag]: kind === 'tag' }, className)}
-      {...rest}
-    >
-      {kind && iconPosition === 'left' && icons[kind]}
+    <PopoverButtonHeadless className={classNames(style.popoverButton, className)} {...rest}>
       {children}
-      {kind && iconPosition === 'right' && icons[kind]}
     </PopoverButtonHeadless>
   );
 };

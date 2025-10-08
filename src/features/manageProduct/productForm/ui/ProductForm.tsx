90068159d5b51710afa866de38089d38d0f3c5ac
Artur <mr.arturio06@gmail.com>
2025-10-08 23:40:54 +0300

    catch errors for product

--- src/features/manageProduct/productForm/ui/ProductForm.tsx
+++ src/features/manageProduct/productForm/ui/ProductForm.tsx
@@ -1,22 +1,40 @@
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { usePostProductMutation, type ProductPostForm } from '@/entities/product';
+import type { ValidationErrorResponse } from '@/shared/api/types/validationErrorResponse';
+import { handleServerErrors } from '@/shared/lib/helpers';
 import { Button } from '@/shared/ui/Button';
 import { Dialog } from '@/shared/ui/Dialog';
 import { Input } from '@/shared/ui/Input';
+import { Text } from '@/shared/ui/Text';
 import style from './ProductForm.module.css';
 
 export const ProductForm = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const { handleSubmit, register } = useForm<ProductPostForm>();
+  const [clientError, setClientError] = useState('');
+  const {
+    handleSubmit,
+    register,
+    setError,
+    clearErrors,
+    formState: { errors },
+  } = useForm<ProductPostForm>();
 
   const [postProductTrigger] = usePostProductMutation();
 
   const onSubmit = async (data: ProductPostForm) => {
+    clearErrors();
     try {
       await postProductTrigger(data).unwrap();
+      setIsOpen(false);
     } catch (error) {
-      console.log(error);
+      const err = error as ValidationErrorResponse;
+      if (err?.data?.name === 'ValidationError') {
+        handleServerErrors(err.data?.list, setError);
+      } else {
+        console.error(error);
+        setClientError('Что-то пошло не так, звоните Артуру :)');
+      }
     }
   };
   return (
@@ -33,11 +51,26 @@ export const ProductForm = () => {
         className={style.productForm}
       >
         <form className={style.form} onSubmit={handleSubmit(onSubmit)}>
-          <Input placeholder="Название культуры" {...register('name')} />
-          <Input type="number" placeholder="Стоимость" {...register('price')} />
-          <Input type="number" placeholder="Кол-во на складе" {...register('quantity')} />
+          <Input
+            placeholder="Название культуры"
+            {...register('name')}
+            error={errors.name?.message}
+          />
+          <Input
+            type="number"
+            placeholder="Стоимость"
+            {...register('price')}
+            error={errors.price?.message}
+          />
+          <Input
+            type="number"
+            placeholder="Кол-во на складе"
+            {...register('quantity')}
+            error={errors.quantity?.message}
+          />
           <Button type="submit">Сохранить</Button>
         </form>
+        {clientError && <Text variant="error">{clientError}</Text>}
       </Dialog>
     </>
   );

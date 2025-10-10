64e53471894b67bf15ed8ed8b2609a60ab1763a3
Artur <mr.arturio06@gmail.com>
2025-10-10 16:24:07 +0300

    added react select

--- src/entities/order/index.ts
+++ src/entities/order/index.ts
@@ -1,1 +1,2 @@
 export { useGetOrderListQuery, usePostOrderMutation } from './api/orderApi';
+export type { OrderPostForm } from './model/types/order';
--- src/features/manageOrder/createOrder/ui/CreateOrder.tsx
+++ src/features/manageOrder/createOrder/ui/CreateOrder.tsx
@@ -1,10 +1,24 @@
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import type { OrderPostForm } from '@/entities/order';
+import { useGetProductsListQuery } from '@/entities/product';
 import { Button } from '@/shared/ui/Button';
 import { Dialog } from '@/shared/ui/Dialog';
+import { Text } from '@/shared/ui/Text';
 import style from './CreateOrder.module.css';
 
 export const CreateOrder = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const { data: productList } = useGetProductsListQuery({});
+  const { handleSubmit } = useForm<OrderPostForm>();
+
+  const onSubmit = async (data: OrderPostForm) => {
+    try {
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
       <Button onClick={() => setIsModalOpen(true)} className={style.width}>
@@ -16,7 +30,15 @@ export const CreateOrder = () => {
         onClose={() => setIsModalOpen(false)}
         closeButton
       >
-        привет
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {productList?.map((product) => {
+            return (
+              <div key={product.id}>
+                <Text>{product.name}</Text>
+              </div>
+            );
+          })}
+        </form>
       </Dialog>
     </>
   );

bd02640d573936d48c6f88045280b0fffcf503ff
Artur <mr.arturio06@gmail.com>
2025-11-27 23:01:18 +0300

    added product count

--- src/features/manageOrder/UploadArchive/ui/UploadArchive.tsx
+++ src/features/manageOrder/UploadArchive/ui/UploadArchive.tsx
@@ -1,5 +1,6 @@
-import * as XLSX from 'xlsx';
+import { utils, writeFile } from 'xlsx';
 import { useLazyGetOrderArchiveQuery } from '@/entities/order';
+import { useGetProductsListQuery } from '@/entities/product';
 import { CURRENCY } from '@/shared/const';
 import { formatDate } from '@/shared/lib/helpers';
 import { Button } from '@/shared/ui/Button';
@@ -7,26 +8,42 @@ import { Button } from '@/shared/ui/Button';
 
 export const UploadArchive = () => {
   const [getArchive] = useLazyGetOrderArchiveQuery({});
+  const { data: productList } = useGetProductsListQuery({ sort: 'name' });
 
   const downloadExcel = async () => {
+    if (!productList) {
+      return;
+    }
     try {
       const resp = await getArchive({ sort: 'deliveryDate' }).unwrap();
-      const workbook = XLSX.utils.book_new();
+      const workbook = utils.book_new();
       const keys = Object.keys(resp);
       keys.forEach((customerName) => {
-        const data = [['Дата доставки', `Стоимость заказа, ${CURRENCY}`]];
+        const data = [
+          [
+            'Дата доставки',
+            `Стоимость заказа, ${CURRENCY}`,
+            ...productList.map((product) => product.name),
+          ],
+        ];
         resp[customerName].forEach((orderData) => {
           const date = formatDate(new Date(orderData.deliveryDate));
           const order = [date, String(orderData.totalPrice)];
+          orderData.products.forEach((product) => {
+            const index = data[0].indexOf(product.name);
+            if (index) {
+              order[index] = String(product.quantity);
+            }
+          });
           data.push(order);
         });
         console.log(resp[customerName]);
-        const worksheet = XLSX.utils.aoa_to_sheet(data);
+        const worksheet = utils.aoa_to_sheet(data);
         worksheet['!cols'] = [{ wch: 15 }, { wch: 18 }];
 
-        XLSX.utils.book_append_sheet(workbook, worksheet, customerName);
+        utils.book_append_sheet(workbook, worksheet, customerName);
       });
-      XLSX.writeFile(workbook, 'example.xlsx');
+      writeFile(workbook, 'example.xlsx');
     } catch (error) {
       console.error('Не удалось загрузить данные с сервера', error);
     }
